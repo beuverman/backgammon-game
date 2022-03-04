983a9a9f6832af27827038293e1dabcd64478181
beuverman <100561304+beuverman@users.noreply.github.com>
2022-03-03 19:43:30 -0800

    Added constructor

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,12 @@ public class Game {
     private Player p2;
     private Board board;
 
+    public Game() {
+        p1 = Player(Color.WHITE);
+        p2 = Player(Color.BLACK);
+        board = Board();
+    }
+
     public void start() {
 
     }
