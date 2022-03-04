cbace7fade6fe744350e9b2a0aa8f67e12eb626d
JohnstonLog <98547883+JohnstonLog@users.noreply.github.com>
2022-03-03 19:23:50 -0800

    Update Bar.java

--- src/Bar.java
+++ src/Bar.java
@@ -1,2 +1,23 @@
 public class Bar {
+    int blackCount;
+    int whiteCount;
+
+    public Bar() {
+      blackCount = 0;
+      whiteCount = 0;
+    }
+
+    public Bar(int blackCount, int whiteCount) {
+        this.blackCount = blackCount;
+        this.whiteCount = whiteCount;
+    }
+
+    public static int getCount(Color){
+        if(Color == BLACK){
+            return blackCount;
+        }else if (Color == Color.WHITE){
+            return whiteCount;
+        }
+        return 0;
+    }
 }
