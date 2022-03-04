00d036eda1c122f018e4d6b4093d4e50591591d2
beuverman <100561304+beuverman@users.noreply.github.com>
2022-03-03 20:16:58 -0800

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,15 @@
 public class Board {
+    private Triangle[] points = new Triangle[24];
+    Bar bar;
+
+    public Board() {
+        bar = new Bar();
+        setInitialBoard();
+    }
+
+    private void setInitialBoard() {
+
+
+        for 
+    }
 }
