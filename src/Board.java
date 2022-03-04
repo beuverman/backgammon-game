640f41f181dd61872554d328d6c400d66bcb0853
beuverman <brandoneuv@gmail.com>
2022-03-04 04:23:51 -0800

    Added initial board state

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,6 @@
 public class Board {
-    private Triangle[] points = new Triangle[24];
-    Bar bar;
+    final private Triangle[] points = new Triangle[24];
+    final Bar bar;
 
     public Board() {
         bar = new Bar();
@@ -8,8 +8,16 @@ public class Board {
     }
 
     private void setInitialBoard() {
-
-
-        for 
+        for (int i = 0; i < points.length; i++) {
+            points[i] = switch (i) {
+                case 0 -> new Triangle(2, Color.BLACK, this);
+                case 5, 12 -> new Triangle(5, Color.WHITE, this);
+                case 7 -> new Triangle(3, Color.WHITE, this);
+                case 11, 18 -> new Triangle(5, Color.BLACK, this);
+                case 16 -> new Triangle(3, Color.BLACK, this);
+                case 23 -> new Triangle(2, Color.WHITE, this);
+                default -> new Triangle(0, Color.EMPTY, this);
+            };
+        }
     }
 }
