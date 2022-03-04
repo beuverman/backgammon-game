4bcb9e4f5bcb8a55c0d86e58b4089db0e2235371
Liam8836 <100892204+Liam8836@users.noreply.github.com>
2022-03-03 19:51:08 -0800

    Update Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,35 @@
 public class Triangle {
+  private int count;
+    private Color color;
+    private Board board;
+
+
+    
+
+    public Triangle(int x,Color y,Board z){
+        count = x;
+        color = y;
+        board = z;
+
+    }
+
+   public int getCount(){
+        return count;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void addPiece(){
+        count = count + 1;
+
+    }
+    public void removePiece(){
+        count = count - 1;
+    }
+
+    public boolean isBlot(){
+        return count == 1;
+    }
 }
