fc686584ce80bc8b446c0fb8e29433eaff4d785b
rikoFuji <98547791+rikoFuji@users.noreply.github.com>
2022-03-03 19:18:00 -0800

    Update ComputerPlayer.java

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,2 +1,19 @@
-public class ComputerPlayer extends Player{
+import java.util.Random;
+
+public class Computer extends Player{
+
+    private String color = "computer";
+    public Computer(String c) {
+        super(c);
+    }
+
+    @Override
+    public int SelectMove(int d1, int d2){
+        int num = Move.getpossibleMoves(color, d1, d2);
+        Random ra = new Random();
+        int move = ra.nextInt(num);
+        //ask the move they want from the possible move
+        return move;
+    }
 }
+
