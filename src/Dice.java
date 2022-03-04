7c640be498c48eba7df45069ef9092b4d37e1b85
JohnstonLog <98547883+JohnstonLog@users.noreply.github.com>
2022-03-03 19:52:39 -0800

    Update Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -1,5 +1,13 @@
 public class Dice {
-  //Random import random secure
-  //methods Roll() should return 2 ints
-  
+  //Import random. 
+  //methods Roll() should return 2 random ints. 
+    private int roll;
+    public Dice() {
+    }
+
+    public int rollDice() {
+        this.roll= (int)(Math.random() * 6 + 1);
+           return roll;
+    }
+    
 }
