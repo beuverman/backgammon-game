9fc83cceda5d0fc940068e892db1b4a14bd42ebb
beuverman <brandoneuv@gmail.com>
2022-03-05 02:45:43 -0800

    Added basic interface

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,33 @@
+import javax.swing.*;
+
 public class Game {
     private Player p1;
     private Player p2;
     private Board board;
 
+    private JFrame frame;
+
     public Game() {
+        board = new Board();
+        setupFrame();
+    }
+
+    private void setupFrame() {
+        frame = new JFrame("Backgammon");
+        frame.setSize(800, 600);
+        frame.setBackground(new java.awt.Color(0.1f, 01.f, 0.3f));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private void getPlayers() {
         p1 = new Player(Color.WHITE);
         p2 = new Player(Color.BLACK);
-        board = new Board();
     }
 
     public void start() {
+        getPlayers();
+
         int r1 = p1.firstRoll();
         int r2 = p2.firstRoll();
         
@@ -22,6 +40,11 @@ public class Game {
     public void Turn(Player active, Player opponent) {
         int[] roll = active.RollTurn();
 
-        Turn(opponent, active);
+        if (!Won(active))
+            Turn(opponent, active);
+    }
+
+    private boolean Won(Player p) {
+        return board.countPieces(p.getColor()) == 0;
     }
 }
