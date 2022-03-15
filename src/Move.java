2b18c55696427c8b39cded61be19824b75dd8764
duncan0714 <98496720+duncan0714@users.noreply.github.com>
2022-03-14 20:09:24 -0700

    Update Move.java

--- src/Move.java
+++ src/Move.java
@@ -1,86 +1,119 @@
 public class Move {
-	int selected;
-	Color color;
-	int x;
+	int pieceCount;
+	PlayerColor pieceColour;
+	int number;
 	int board;
+	int dice[];
+	int blackCount;
+	int whiteCount;
 	
 	
 	public Move() {
-		selected = 0;
+		pieceCount = 0;
   }
 
-	public Triangle[] getPossibleMoves(int[] dice, int selected, int board, Color color, int x) {
-		switch(color){
+	public Object getPossibleMoves() {
+		switch(pieceColour){
 			case WHITE:
-				if (Bar.whiteCount > 0) {
-					if(Triangle(1, Color.BLACK, (24 - dice[0] + 1)))
-						return Triangle(1, Color.BLACK, (24 - dice[0] + 1));
-					if(Triangle(x, Color.WHITE, (24 - dice[0] + 1)))
-						return Triangle(x, Color.WHITE, (24 - dice[0] + 1));
-					if(Triangle(0, Color.EMPTY, (24 - dice[0] + 1)))
-						return Triangle(0, Color.EMPTY, (24 - dice[0] + 1));
+				if (whiteCount > 0) {
+					if((boolean) Triangle(1, PlayerColor.BLACK, (24 - dice[0] + 1)))
+						return Triangle(1, PlayerColor.BLACK, (24 - dice[0] + 1));
+					if((boolean) Triangle(pieceCount, PlayerColor.WHITE, (24 - dice[0] + 1)))
+						return Triangle(pieceCount, PlayerColor.WHITE, (24 - dice[0] + 1));
+					if((boolean) Triangle(0, null, (24 - dice[0] + 1)))
+						return Triangle(0, null, (24 - dice[0] + 1));
 						
-					if(Triangle(1, Color.BLACK, (24 - dice[1] + 1)))
-						return Triangle(1, Color.BLACK, (24 - dice[1] + 1));
-					if(Triangle(x, Color.WHITE, (24 - dice[1] + 1)))
-						return Triangle(x, Color.WHITE, (24 - dice[1] + 1));
-					if(Triangle(0, Color.EMPTY, (24 - dice[1] + 1)))
-						return Triangle(0, Color.EMPTY, (24 - dice[1] + 1));
+					if((boolean) Triangle(1, PlayerColor.BLACK, (24 - dice[1] + 1)))
+						return Triangle(1, PlayerColor.BLACK, (24 - dice[1] + 1));
+					if((boolean) Triangle(pieceCount, PlayerColor.WHITE, (24 - dice[1] + 1)))
+						return Triangle(pieceCount, PlayerColor.WHITE, (24 - dice[1] + 1));
+					if((boolean) Triangle(0, null, (24 - dice[1] + 1)))
+						return Triangle(0, null, (24 - dice[1] + 1));
 				}
-				else if (Bar.whiteCount = 0) {
-					if(Triangle(1, Color.BLACK, (selected - dice[0])))
-						return Triangle(1, Color.BLACK, (selected - dice[0]));
-					if(Triangle(x, Color.WHITE, (selected - dice[0])))
-						return Triangle(x, Color.WHITE, (selected - dice[0]));
-					if(Triangle(0, Color.EMPTY, (selected - dice[0])))
-						return Triangle(0, Color.EMPTY, (selected - dice[0]));
+				else  {
+					if((boolean) Triangle(1, PlayerColor.BLACK, (number - dice[0])))
+						return Triangle(1, PlayerColor.BLACK, (number - dice[0]));
+					if((boolean) Triangle(pieceCount, PlayerColor.WHITE, (number - dice[0])))
+						return Triangle(pieceCount, PlayerColor.WHITE, (number - dice[0]));
+					if((boolean) Triangle(0, null, (number - dice[0])))
+						return Triangle(0, null, (number - dice[0]));
 					
-					if(Triangle(1, Color.BLACK, (selected - dice[1])))
-						return Triangle(1, Color.BLACK, (selected - dice[1]));
-					if(Triangle(x, Color.WHITE, (selected - dice[1])))
-						return Triangle(x, Color.WHITE, (selected - dice[1]));
-					if(Triangle(0, Color.EMPTY, (selected - dice[1])))
-						return Triangle(0, Color.EMPTY, (selected - dice[1]));
+					if((boolean) Triangle(1, PlayerColor.BLACK, (number - dice[1])))
+						return Triangle(1, PlayerColor.BLACK, (number - dice[1]));
+					if((boolean) Triangle(pieceCount, PlayerColor.WHITE, (number - dice[1])))
+						return Triangle(pieceCount, PlayerColor.WHITE, (number - dice[1]));
+					if((boolean) Triangle(0, null, (number - dice[1])))
+						return Triangle(0, null, (number - dice[1]));
 				}
 				
 			case BLACK:
-				if (Bar.blackCount > 0) {
-					if(Triangle(1, Color.WHITE, dice[0]))
-						return Triangle(1, Color.WHITE, dice[0]);
-					if(Triangle(x, Color.BLACK, dice[0]))
-						return Triangle(x, Color.BLACK, dice[0]);
-					if(Triangle(0, Color.EMPTY, dice[0]))
-						return Triangle(0, Color.EMPTY, dice[0]);
+				if (blackCount > 0) {
+					if((boolean) Triangle(1, PlayerColor.WHITE, dice[0]))
+						return Triangle(1, PlayerColor.WHITE, dice[0]);
+					if((boolean) Triangle(pieceCount, PlayerColor.BLACK, dice[0]))
+						return Triangle(pieceCount, PlayerColor.BLACK, dice[0]);
+					if((boolean) Triangle(0, null, dice[0]))
+						return Triangle(0, null, dice[0]);
 						
-					if(Triangle(1, Color.WHITE, dice[1]))
-						return Triangle(1, Color.WHITE, dice[1]);
-					if(Triangle(x, Color.BLACK, dice[1]))
-						return Triangle(x, Color.BLACK, dice[1]);
-					if(Triangle(0, Color.EMPTY, dice[1]))
-						return Triangle(0, Color.EMPTY, dice[1]);
+					if((boolean) Triangle(1, PlayerColor.WHITE, dice[1]))
+						return Triangle(1, PlayerColor.WHITE, dice[1]);
+					if((boolean) Triangle(pieceCount, PlayerColor.BLACK, dice[1]))
+						return Triangle(pieceCount, PlayerColor.BLACK, dice[1]);
+					if((boolean) Triangle(0, null, dice[1]))
+						return Triangle(0, null, dice[1]);
 				}
-				else if (Bar.blackCount = 0) {
-					if(Triangle(1, Color.WHITE, (selected + dice[0])))
-						return Triangle(1, Color.WHITE, (selected + dice[0]));
-					if(Triangle(x, Color.BLACK, (selected + dice[0])))
-						return Triangle(x, Color.BLACK, (selected + dice[0]));
-					if(Triangle(0, Color.EMPTY, (selected + dice[0])))
-						return Triangle(0, Color.EMPTY, (selected + dice[0]));
+				else {
+					if((boolean) Triangle(1, PlayerColor.WHITE, (number + dice[0])))
+						return Triangle(1, PlayerColor.WHITE, (number + dice[0]));
+					if((boolean) Triangle(pieceCount, PlayerColor.BLACK, (number + dice[0])))
+						return Triangle(pieceCount, PlayerColor.BLACK, (number + dice[0]));
+					if((boolean) Triangle(0, null, (number + dice[0])))
+						return Triangle(0, null, (number + dice[0]));
 					
-					if(Triangle(1, Color.WHITE, (selected + dice[1])))
-						return Triangle(1, Color.WHITE, (selected + dice[1]));
-					if(Triangle(x, Color.BLACK, (selected + dice[1])))
-						return Triangle(x, Color.BLACK, (selected + dice[1]));
-					if(Triangle(0, Color.EMPTY, (selected + dice[1])))
-						return Triangle(0, Color.EMPTY, (selected + dice[1]));
+					if((boolean) Triangle(1, PlayerColor.WHITE, (number + dice[1])))
+						return Triangle(1, PlayerColor.WHITE, (number + dice[1]));
+					if((boolean) Triangle(pieceCount, PlayerColor.BLACK, (number + dice[1])))
+						return Triangle(pieceCount, PlayerColor.BLACK, (number + dice[1]));
+					if((boolean) Triangle(0, null, (number + dice[1])))
+						return Triangle(0, null, (number + dice[1]));
 				}
 			}
+		return null;
 	}
 	
 
 
-	public void makeMove(int[] dice, int selected, int board, Color color, int x) {
-		
+	private Object Triangle(int i, Object object, int j) {
+		pieceCount=i;
+		object=PlayerColor.class;
+		j=board;
+		return Triangle(i, object, j);
+	}
+
+	public void makeMove() {
+		getPossibleMoves();
+		switch(pieceColour) {
+			case WHITE:
+				if((boolean) Triangle(1, PlayerColor.BLACK, board)) {
+					Triangle(pieceCount = 1, PlayerColor.WHITE, board);
+					++blackCount;
+				}
+				if((boolean) Triangle(pieceCount, PlayerColor.WHITE, board))
+					Triangle(++pieceCount, PlayerColor.WHITE, board);
+				if((boolean) Triangle(0, null, board))
+					Triangle(1, PlayerColor.WHITE, board);
+			
+			case BLACK:
+				if((boolean) Triangle(1, PlayerColor.WHITE, board)) {
+					Triangle(pieceCount = 1, PlayerColor.BLACK, board);
+					++whiteCount;
+				}
+				if((boolean) Triangle(pieceCount, PlayerColor.BLACK, board))
+					Triangle(++pieceCount, PlayerColor.BLACK, board);
+				if((boolean) Triangle(0, null, board))
+					Triangle(1, PlayerColor.BLACK, board);
+			
+		}
 		
 	}
 
