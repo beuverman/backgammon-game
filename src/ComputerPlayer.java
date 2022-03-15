eae97a1aa8b3a1f3be44a37180d8c9624e53dc67
rikoFuji <98547791+rikoFuji@users.noreply.github.com>
2022-03-14 19:57:38 -0700

    Update ComputerPlayer.java

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -2,6 +2,18 @@ public class ComputerPlayer extends Player{
     public ComputerPlayer(PlayerColor c, String name) {
         super(c, name);
     }
+    @Override
+    public void SelectMove(Turn[] turns) {
 
+        int count = Move.getPossibleMoves().length;
+        Random randomNum = new Random();
+        int num = randomNum.nextInt(turns.length-1);
+
+                for (Move m: turns[num]){
+                    Board.makeMove(m);
+                }
+
+
+    }
 }
 
