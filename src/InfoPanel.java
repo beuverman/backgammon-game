02abbaa6862f8aac1fa6b4ae767bb3dbe33763d4
Liam8836 <100892204+Liam8836@users.noreply.github.com>
2022-03-15 14:25:47 -0700

    Update InfoPanel.java

--- src/InfoPanel.java
+++ src/InfoPanel.java
@@ -4,20 +4,30 @@ public class InfoPanel extends JPanel {
     private Game game;
     private JLabel rolls;
     private JLabel activePlayer;
+    private JLabel scores;
 
     public InfoPanel(Game game) {
         this.game = game;
 
         activePlayer = new JLabel();
         rolls = new JLabel();
+        scores = new JLabel();
 
         add(activePlayer);
         add(rolls);
+        add(scores);
     }
 
     public void updateInfo() {
         int[] r = game.getRolls();
         StringBuilder rollText = new StringBuilder("Moves: ");
+        int[] s = game.getScores();
+        StringBuilder scoreText = new StringBuilder("Score: ");
+        
+        for(int score : s){
+            scoreText.append(score);
+            scoreText.append(", ");
+        }
 
         for (int roll : r){
             rollText.append(roll);
@@ -25,6 +35,7 @@ public class InfoPanel extends JPanel {
         }
         rollText.setLength(rollText.length() - 2);
 
+        scores.setText(scoreText.toString());
         rolls.setText(rollText.toString());
         activePlayer.setText(game.getActivePlayer().getName());
 
